804b9ce9545bd05264ee1740e551e066669f98e1
John Nishinaga <jingoro@casa-z.org>
2012-06-11 22:36:24 -0400

    add hello-josh.c to start C programming

---
+++ c/hello-josh.c
@@ -0,0 +1,12 @@
+/*
+ * Comple & run with:
+ *
+ *   gcc hello-josh.c -o hello-josh && ./hello-josh
+ */ 
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+   printf("Hello Josh!\n");
+   return 0;
+}
