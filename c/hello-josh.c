94ac1b7a32a08ad4a763c5399a5872c3cd89a2ff
John Nishinaga <jingoro@casa-z.org>
2012-06-11 22:39:41 -0400

    add instructions for Josh

--- c/hello-josh.c
+++ c/hello-josh.c
@@ -1,4 +1,12 @@
 /*
+ * Your assignment:
+ *
+ * 1. Grab these changes into your local repository.
+ * 2. Compile and run this file. (See below)
+ * 3. Add another print statement with a response from you.
+ * 4. Check in your changes and push up the changes back to github.
+ * 5. Get John and LED and USB cable.
+ *
  * Comple & run with:
  *
  *   gcc hello-josh.c -o hello-josh && ./hello-josh
